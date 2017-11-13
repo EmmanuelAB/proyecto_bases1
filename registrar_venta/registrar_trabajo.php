5e9dde753e59f2072fbbb4c6f7612fd3940c37cb
Emmanuel Alfaro <ealfarobrenes@hotmail.com>
2017-11-12 19:48:58 -0600

    listo reg_trabajo

--- registrar_venta/registrar_trabajo.php
+++ registrar_venta/registrar_trabajo.php
@@ -1,16 +1,16 @@
 <?php 
     require("../base/credenciales.php");
+    $fecha = $_GET["c_fecha"];
+    $cedula = $_GET["c_cedula"];
+    $descrip = $_GET["c_descrip"];
+    $precio = $_GET["c_precio"];
     
     $con = pg_connect($datos_conexion) or die ('No se ha podido conectar'. pg_last_error() );
-    $res = pg_query("select concat(c.nombre,' ', c.apellido1) from cliente c ;");
-            //echo "<select style='margin: 20px 10px;'>";
-            while($registro = pg_fetch_array($res, null, PGSQL_ASSOC)){
-                foreach($registro as $campo){
-                    echo "<option value='$campo'>";
-                    echo  $campo;
-                    echo "</option>";
-                }
-            }
-            //echo "</select>";
+    $res = pg_query("insert into entradaexp(identradaexp, fecha , descripcion, cantidad, total, cedula)".
+                    "values (41,'$fecha','$descrip',1,$precio,$cedula);"
+                    );
+    if($res){
+        echo "Registro exitoso";
+    }
     pg_close($con);
 ?>
