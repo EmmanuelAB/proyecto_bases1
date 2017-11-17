077b91e45ee0a53315ed1ffcdfea23fed27bef57
Emmanuel Alfaro <ealfarobrenes@hotmail.com>
2017-11-16 18:47:07 -0600

    soluciona errores de rutas

--- Insertar/Ligas(Producto, Tipo Carro)/ligar.php
+++ Insertar/Ligas(Producto, Tipo Carro)/ligar.php
@@ -1,5 +1,7 @@
 <?php
-    require("../base/credenciales.php");
+    require("../../Base/CredencialesProyecto.php");
+    $datos_conexion = "host=".$db_direction." dbname=".$db_name. " user=".$db_username. " password=".$db_password;
+
     $con = pg_connect($datos_conexion) or die ('No se ha podido conectar'. pg_last_error() );
     $carro = $_GET["c_carro"];
     $codigo = $_GET["c_repuesto"];
@@ -9,10 +11,13 @@
                     "(idtipocarro, codigoproducto, precio, cantidad) values".
                     "($carro,$codigo,$precio,$cantidad);");
     if($res){
-        echo "Exito";
+        echo "<link rel='stylesheet' type='text/css' href='../../Estilos/estilo_header_comun.css'>";
+        echo '<link rel="stylesheet" type="text/css" href="../../Estilos/titulo_pagina.css">';
+        echo "<iframe src='../../Plantillas/header_comun/header_comun.html'></iframe>";
+        echo "<center><p id='titulo_pagina'>Inserción exitosa!</p></center>";
     }
     else{
-        echo "Error en la insercion";
+        echo "<h1>Insercion fallida!</h1>";
     }
     pg_close($con);
 ?>
--- Insertar/Ligas(Producto, Tipo Carro)/pedir_repuestos.php
+++ Insertar/Ligas(Producto, Tipo Carro)/pedir_repuestos.php
@@ -1,5 +1,6 @@
 <?php
-    require("../base/credenciales.php");
+    require("../../Base/CredencialesProyecto.php");
+    $datos_conexion = "host=".$db_direction." dbname=".$db_name. " user=".$db_username. " password=".$db_password;
     $con = pg_connect($datos_conexion) or die ('No se ha podido conectar'. pg_last_error() );
     $res = pg_query("select codigo, nombre from producto;");
             while($registro = pg_fetch_array($res, null, PGSQL_ASSOC)){
